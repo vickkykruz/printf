ba345f2f7b15aa7607614e6ec21ff78d5b2677b4
seetafang <mkpoutoobot321@gmail.com>
2023-06-27 20:57:32 -0700

    changes to putchar

--- _putchar.c
+++ _putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _putchar - This is a function that writes out character to the command line
+ * _putchar - A function that writes out character to the command line
  * using stdout
- * @c: This is an argument that represent the given charater
+ * @c: An argument that represent the given charater
  *
- * Return: This function return (int)
+ * Return: Function returns (int)
  */
 int _putchar(char c)
 {
